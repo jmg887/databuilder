6a40aa67982ab6f77c3b1b0639524a38ab52c5ff
Farmsky <farmskyventures@gmail.com>
2026-09-04 19:24:41 +0300

    Fix RDS backup retention for free tier compatibility

--- infra/rds.tf
+++ infra/rds.tf
@@ -26,7 +26,7 @@ resource "aws_db_instance" "postgres" {
   publicly_accessible    = false
 
   multi_az                = false
-  backup_retention_period = 7
+  backup_retention_period = 1
   skip_final_snapshot     = true
   deletion_protection     = false
 
